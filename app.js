6f86cc5b61f0767c2145143ba12fbdfea54d7bd2
zhebil <yevhenii.bilyk@flexum.io>
2023-11-27 18:13:58 +0200

    Add ping endpoint

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const mafiaRoute = require('./routes/mafia.routes');
 const pokerRoute = require('./routes/poker.routes');
-const cors = require('cors');
+
 const PORT = parseInt(process.env.PORT, 10) || 3001;
 
 async function run(app) {
   try {
     app.listen(PORT, () => {
-      console.log(`app started`);
+      console.log('app started');
     });
   } catch (e) {
     console.log(e);
@@ -21,6 +22,7 @@ function runExpressApp() {
   const app = express();
   app.use(cors());
   app.use(express.json({ extended: true }));
+  app.use('/ping', (req, res) => res.json({ message: 'pong' }));
   app.use('/api/mafia', mafiaRoute);
   app.use('/api/poker', pokerRoute);
 
